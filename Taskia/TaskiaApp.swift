f2d364d9c62c788636bdf43bdb81cac293d0c7b6
Serhii Cherepiuk <s.cherepiuk@gmail.com>
2023-06-10 13:30:13 +0200

    Initial Commit

---
+++ Taskia/TaskiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskiaApp.swift
+//  Taskia
+//
+//  Created by Serhii Cherepiuk on 10/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
